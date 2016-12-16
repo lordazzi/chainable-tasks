96520a047f8a84ff0f8e7b9638fa25e7e100ef7e
Ricardo Azzi <ricardo.azzi@viavarejo.com.br>
2016-12-16 21:54:43 -0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,111 @@
-# tasks
-Like native javascript promises, this library is designed to work with a large number of chained asynchronous requests involved with business rules and data return.
+# Tasks
+Like native JavaScript promises, this library is designed to work with a large number of chained asynchronous requests involved with business rules and data return in a simple way.
+
+Assim como os promises nativos de JavaScript, esta biblioteca foi desenhada para trabalhar com uma grande quantidade de requisições assíncronas encadeadas envolvendo regras de negócio e retorno de dados de uma forma simples.
+
+    var tarefas = new Tasks.Task(function(yes, no){
+         setTimeout(function(){
+         	yes('love potatoes');
+         }, 1000);
+    }).then(function(yes, no, anything){
+         console.info(anything);
+         setTimeout(function(){
+         	yes(anything + ' and bacon');
+         }, 1000);
+    }).then(function(yes, no, anything){
+         console.info(anything);
+         setTimeout(function(){
+         	throw "error message";
+         }, 1000);
+    });
+    
+    //  see that the next task will be bind to the first one generated,
+    //  but this will not change the execution order, internally this
+    //  new task will be created as the last task son
+    tarefas.then(function(yes, no, anything){
+
+         // this one never will be executed because the last task is
+         // lauching an exception so, the catch will be activated 
+         console.info(anything);
+         setTimeout(function(){
+         	yes(anything + ' and onion rings');
+         }, 1000);
+    }).catch(function(err){
+         console.error(err.message);
+    });
+
+Add new tasks in the end using the original task or some one other in the middle will help you join multiples requests and return something like a promise with data access to the programmer continue working with tasks, like this:
+
+    var dao = {
+        getProduct: (data) => {
+            return new Tasks.Task((next, stop) => {
+    
+                $.ajax({
+                    url:    "product/infos"
+    
+                    data:   data,
+    
+                    method: 'GET',
+    
+                    success(infos){
+                        next({ 
+                            type:         infos.type,
+                            name:         infos.name,
+                            description:  infos.description
+                        });
+                    },
+    
+                    failure(err){
+                        stop(err);
+                    }
+                });
+    
+            }).then(function(next, stop, dataFromOtherRequest){
+    
+                $.ajax({
+                    url:    "product/images"
+                    
+                    data:   { type: dataFromOtherRequest.type },
+    
+                    method: 'GET',
+    
+                    success(prices){
+                        dataFromOtherRequest.prices = prices;
+                        next(dataFromOtherRequest);
+                    },
+    
+                    failure(err){
+                        stop(err);
+                    }
+                });
+    
+            }).then(function(next, stop, dataFromOtherRequest){
+    
+                $.ajax({
+                    url:    "product/images"
+                    
+                    data:   data,
+    
+                    method: 'GET',
+    
+                    success(images){
+                        dataFromOtherRequest.images = images;
+                        next(dataFromOtherRequest);
+                    },
+    
+                    failure(err){
+                        stop(err)
+                    }
+                });
+    
+            });
+        }
+    };
+    
+    dao.getProduct({ code: 1000 }).then((go, dontgo, data) => {
+    
+        // display data in screen or anothor stuff
+    
+    }).catch(e => {
+        alert(e.message);
+    });
---
+++ src/Tasks.ts
@@ -0,0 +1,173 @@
+/**
+ * Biblioteca de tarefas encadeaveis para melhorar a programação de conjuntos de
+ * requisições assíncrona aninhadas com regras de negócio
+ */
+export namespace Tasks {
+    enum TaskStatus {
+        CREATED,
+        WAITING,
+        RUNNING,
+        FULFILLED,
+        REJECTED
+    }
+
+    class TaskArgument {
+        public constructor(public parent: Task, public callback: ThenCallback) {
+
+        }
+    }
+
+    type ThenCallback       = (resolve: (any) => void, reject: (Error) => void, data: any) => Task;
+    type CatchCallback      = (err: Error, data: any) => void;
+    type ChildCallback      = (data: any, err: Error) => void; 
+
+    export class Task {
+
+        /**
+         * Situação atual da tarefa
+         */
+        public status: TaskStatus = TaskStatus.CREATED;
+
+        /**
+         * Função que representa o corpo da tarefa a ser executado
+         */
+        private thenCallback: ThenCallback;
+
+        /**
+         * Função que será chamada caso tudo dê errado
+         */
+        private catchCallback: CatchCallback;
+
+        /**
+         * Dados vinculadas com as tarefas, eles precisam vir juntos
+         * caso haja a necessidade de retorná-la em um contexto onde
+         * ela esteja encapsulada
+         */
+        private data: any = null;
+
+        /**
+         * Quando um erro é lançado na execução do corpo de uma tarefa
+         * ou então quando o reject é executado
+         */
+        private err: Error = null;
+
+        /**
+         * Tarefa precedente a esta
+         */
+        private parent: Task;
+
+        /**
+         * Tarefa seguinte a esta
+         */
+        private child: Task;
+
+        /**
+         * Aqui serão vinculadas todas as funções que devem ser vinculadas
+         * quando a requisição for concluida
+         */
+        private childCry: Array<ChildCallback> = new Array<ChildCallback>();
+
+        public constructor(args: ThenCallback|TaskArgument) {
+            if (args instanceof TaskArgument) {
+                this.thenCallback   = args.callback;
+                this.parent         = args.parent;
+
+                switch (this.parent.status) {
+                    case TaskStatus.FULFILLED:
+                        this.executeCallback();
+                        break;
+
+                    case TaskStatus.REJECTED:
+                        this.status = TaskStatus.REJECTED;
+                        break;
+
+                    default:
+                        this.status = TaskStatus.WAITING;
+                        this.parent.onComplete((data, err) => {
+                            this.data   = data;
+                            this.err    = err;
+
+                            if (this.parent.status == TaskStatus.REJECTED)
+                                this.status = TaskStatus.REJECTED;
+
+                            this.executeCallback();
+                        });
+                        break;
+
+                }
+            } else {
+                this.thenCallback = <ThenCallback> args;
+                this.executeCallback();
+            }
+        }
+
+        public getParent(): Task {
+            return this.parent;
+        }
+
+        public getChild(): Task {
+            return this.child;
+        }
+
+        private resolve(data: any): void {
+            if (this.status == TaskStatus.FULFILLED || this.status == TaskStatus.REJECTED)
+                return;
+
+            this.status = TaskStatus.FULFILLED;
+            this.data = data;
+
+            this.complete();
+        }
+
+        private reject(err: Error): void {
+            if (this.status == TaskStatus.FULFILLED || this.status == TaskStatus.REJECTED)
+                return;
+
+            this.status = TaskStatus.REJECTED;
+            this.err = err;
+
+            this.complete();
+        }
+
+        private executeCallback() {
+            try {
+                this.status = TaskStatus.RUNNING;
+
+                if (this.parent && this.parent.status === TaskStatus.REJECTED) {
+                    if (this.catchCallback) this.catchCallback(this.err, this.data);
+                } else {
+                    if (this.thenCallback) this.thenCallback(data => this.resolve(data), err => this.reject(err), this.data);
+                }
+            } catch (e) {
+                this.status = TaskStatus.REJECTED;
+                this.reject(e);
+            }
+        }
+
+        public then(callback: ThenCallback): Task {
+            let noviceTask: Task    = this;
+            let baby                = new Task(new TaskArgument(this, callback));
+
+            if (!this.child)
+                return this.child = baby;
+
+
+            while (noviceTask.getChild())
+                noviceTask = noviceTask.getChild();
+
+            return noviceTask.then(callback);
+        }
+
+        public catch(catchCallback: CatchCallback): void {
+            this.catchCallback = catchCallback;
+        }
+
+        public onComplete(call: ChildCallback): void {
+            this.childCry.push(call);
+        }
+
+        private complete() {
+            this.childCry.forEach(callback => callback(this.data, this.err));
+        }
+    }
+}
